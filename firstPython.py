641a0ed03feb12b8921decd59405df1d226efa66
Nandu Anil <nandua.dev@gmail.com>
2017-01-25 23:19:33 -0800

    basic programs

---
+++ firstPython.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 20:58:10 2017
+
+@author: nandu
+"""
+
+#%%
+def first():
+    print("Hello World")
+
+
+
+#%%
+def areaTriangle(base,height):
+    area = (1/2)*base*height
+    print("The area of triangle of base",base,"and height",height,"is:",area) 
+
+#%%
+def fahrenheit(celcius):
+    f = 1.8 * celcius +32
+    print(celcius," degree celcius \in '\"fahrenheit is:",f)
+
+
+
+#%%
